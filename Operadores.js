7219e4dea3ff2c46b860f64cd5787c939e8deaa8
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-13 19:17:08 -0500

    Operadores logicos script

---
+++ Operadores.js
@@ -0,0 +1,20 @@
+//Operadores
+        //Aritmeticos
+        let a = 5 + 5;
+        let modulo = 5 % 2;
+
+
+        console.log(a)
+        console.log(modulo)
+
+        //Operadores relacionales
+        // < >
+        console.log(8 <= 7)
+        console.log('7' === 7)
+        console.log('7' == 7)
+
+        //Incremento
+        let i = 0;
+        i++;
+        i *= 2;
+        console.log(i)
