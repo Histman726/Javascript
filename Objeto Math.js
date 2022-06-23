c76b6c15bccf23919bc979b1d204635986ff28ba
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-23 12:24:09 -0500

    Objeto Math
    Signed-off-by: Histman726 <hsandovaltovar@alumno.ipn.mx>

---
+++ Objeto Math.js
@@ -0,0 +1,16 @@
+//Objeto Math
+        console.log(Math)
+        console.log(Math.PI)
+
+        //Redondeo de numeros
+        console.log(Math.ceil(7.2))
+        console.log(Math.floor(9.2))
+        console.log(Math.round(5.5))
+
+        //Operaciones
+        console.log(Math.sqrt(16))
+        console.log(Math.pow(2, 6))
+        console.log(Math.sign(-12))// -1 ya que es un numeronegativo
+
+        //Numero aleatorio entre 1 y 100
+        console.log(Math.round(Math.random() * 100))
