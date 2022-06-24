cb505040d20c9b52cde94e4a506882b8393ead35
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-24 12:22:14 -0500

    Objeto window
    Signed-off-by: Histman726 <hsandovaltovar@alumno.ipn.mx>

---
+++ Window.js
@@ -0,0 +1,10 @@
+//Alert, confirm y prompt
+
+        //Alert: undefined
+        //alert("Hola soy una alerta")
+
+        //Confirm: boolean
+        //confirm("Hola esto es una confirmacion")
+
+        //Prompt: String
+        //prompt("Hola esto es un aviso con input")
