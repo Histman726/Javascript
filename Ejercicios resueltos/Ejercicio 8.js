612e4c2d3d20165b18ab66107f8539e84171ab6c
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-27 17:56:51 -0500

    Ejercicios resueltos 2.2
    Signed-off-by: Histman726 <hsandovaltovar@alumno.ipn.mx>

---
+++ Ejercicios resueltos/Ejercicio 8.js
@@ -0,0 +1,10 @@
+//8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+
+const eliminarCaracteres=(texto="",patron="")=>
+    (!texto)
+        ? console.warn("No ingresaste un texto")
+        : (!patron)
+            ? console.warn("No ingresaste un patron")
+            : console.info(texto.replace(new RegExp(patron,"ig"),""))
+
+eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5","xyz")
