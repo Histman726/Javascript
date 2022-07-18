2bbbc3af27ca0cde8d68646feefee19127121b4b
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-07-18 17:49:40 -0500

    Jquery: Primeros pasos
    Signed-off-by: Histman726 <hsandovaltovar@alumno.ipn.mx>

---
+++ Jquery/Selectores.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // let buttun=$('button').html();
+    // console.log(buttun)
+    // $('button').html("Texto Cambiado");
+    //$('#Boton').html("Cambiado desde el Id");
+    //$('.btn').html("Cambiado desde la clase");
+    //$('#Subcontenedor button').html("Cambiado desde subcontenedor")
+    $('button:first').html("Primero");
+    $('button:last').html("Ultimo");
+});
