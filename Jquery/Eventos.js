10fde140d68583ae888836d5facf75466cda07b6
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-07-25 20:23:06 -0500

    Evento click

---
+++ Jquery/Eventos.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+    $('#boton').click(function (e){
+        e.preventDefault()
+        $('img').attr('src', 'logo2.png');
+    })
+    $('#subcontenedor button:nth-child(1)').click(function (e){
+        e.preventDefault()
+        $('img').attr('width', '70px');
+    })
+    $('#subcontenedor button:nth-child(2)').click(function (e){
+        e.preventDefault()
+        let margen=$('img').css('margin-left')
+        margen=parseInt(margen.replace('px',''))
+        margen+=30
+        $('img').css('margin-left',margen+'px')
+    })
+    $('#subcontenedor button:nth-child(3)').click(function (e){
+        e.preventDefault()
+        $('img').toggleClass('d-none')
+    })
+})
